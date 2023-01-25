cf61820ad18c8071505416e26f53a62b44f83b13
Markin Konstantin <markin_konstantin@vk.com>
2023-01-25 22:36:15 +0500

    update

---
+++ Python/PyhtonLecture/les1/classwork/task2/task2.py
@@ -0,0 +1,182 @@
+# value = None
+# # print(type(value))
+# a = 123
+# b = 1.23
+# # print (type(a))
+# # print(type(b))
+# value = 12341
+# # print(value)
+# # print(type(value))
+# s = " 'hello  world'" 
+# print(s)
+
+# print ( a, '-', b, '-', s)
+# print ('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+ 
+
+# f = True
+# print(f)
+
+# list = ['1', '2', '3']
+# col = ['hello', 1,2,3,4.5,True]
+# print(list)
+# print(col)
+
+# print('Введите а ')
+# a = float(input())
+# print('Введите b ')
+# b = float(input())
+
+# print(a, '+', b, '=', a + b)
+# print('{} {}'.format(a,b))
+# print(f'{a} {b}')
+
+# a = 1.3234265656555656
+# b = 3
+
+# c = round (a * b, 5)
+
+# print(c)
+
+
+# a = 3
+# a +=5
+
+# print(a)
+
+
+# a = 1 > 4 or 5 > 2
+# print (a)
+
+
+# func  = 1
+# T = 4
+# x = 2
+
+
+# print(func<T>(x))
+
+# f =  [1, 2, 3, 4]
+
+# print(f)
+# print(not 2 in f)
+
+# is_odd =  not f[0] % 2 == 0
+# print(is_odd)
+
+
+# username = input ('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же Маша')
+# elif username == 'Марина':
+#     print('привет, марина')
+# elif username == 'Ильдар':
+#     print('Ильдар - топ ')
+# else:
+#     print('Привет, ', username)
+
+
+
+# original = 23
+# interverted = 0
+
+# while original !=0:
+#     interverted = interverted * 10 + (original %10)
+#     original //=10
+#     print(original)
+# else:
+#     print('Хватит')
+#     print('На сегодня')
+# print(interverted)
+
+
+# r = range (1, 10, 2)
+
+# for i in r:
+#     print(i)
+
+
+
+
+
+
+
+
+# text = 'съешь ещё этиъ мягких французких булок'
+
+
+# print(len(text))
+# print(text[0])
+# print(text[-1])
+# print(text[0:2])
+# print(text[len(text)-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# numbers = [1,2,3,4,5]
+# print(numbers)
+# ran = range(1,6)
+# print(range)
+
+
+# print(type(numbers))
+
+
+# print(type(numbers))
+# print(numbers)
+# numbers[0] = 10
+# print(f'{len(numbers)} len')
+
+# for i in numbers:
+#     i *= 2
+#     print(i)
+# print(numbers)
+
+
+
+
+
+
+
+
+# colors = ['red', 'green', 'blue']
+
+# for e in colors:
+#     print(e)
+
+# for e in colors:
+#     print(e*2)
+
+# colors.append('gray')
+
+# print(colors == ['red', 'green', 'blue', 'gray'])
+# colors.remove('red')
+
+# del
+
+
+def f (x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+
+
+arg = 222
+print (f(arg))
+print (type(f(arg)))
